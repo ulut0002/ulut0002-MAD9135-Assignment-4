623e1404e78380fb4e9214807b5d1b2e87cb0c77
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-11-26 18:20:52 -0500

    Initialize

---
+++ lib/screens/data_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DataScreen
+ extends StatefulWidget {
+  const DataScreen
+  ({super.key});
+
+  @override
+  State<DataScreen
+  > createState() => _DataScreen
+  State();
+}
+
+class _DataScreen
+State extends State<DataScreen
+> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
