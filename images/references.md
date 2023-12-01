b39c620595fdca81e0c829f998d01675b0dc35b4
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-12-01 11:51:59 -0500

    Add sample image. Convert mainpage to statesless widget. Complete first page without text styles

---
+++ images/references.md
@@ -0,0 +1,1 @@
+https://unsplash.com/photos/brown-desert-under-blue-sky-2BMSj-xN008
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,25 +1,44 @@
+import 'dart:ffi';
+import "dart:math";
+
 import 'package:flutter/material.dart';
 import 'package:flutter2/widgets/navigation_bar.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key, required this.nav, required this.page});
 
   final Function nav;
   final int page;
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Center(child: Text("Home Screen")),
-      bottomNavigationBar: NavBar(nav: widget.nav, page: widget.page),
+      body: Column(
+        children: [
+          Image.asset("images/homeImage.jpeg"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.black87,
+              ),
+              child: Transform.rotate(
+                angle: 15 * (pi / 180),
+                child: const Text(
+                  "Hello",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: NavBar(nav: nav, page: page),
     );
   }
 }
